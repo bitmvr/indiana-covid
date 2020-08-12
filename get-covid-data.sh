b3a3d3c1a9d2db88835da50cf79d1ca065124dff
Jesse Riddle <jriddle@salesforce.com>
2020-08-12 08:51:41 -0400

    Removed superfluous comments.

--- get-covid-data.sh
+++ get-covid-data.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# For the national data, add NULL_FIELD to 'empty' columns so that the cut is performed correctly
-# sed -i -- "s/,,/,NULL_FIELD,/g" ts-covid-us-null-filled.csv
-# cat ./ts-covid-us.csv | cut -d ',' -f6-7,12- > ./filtered.csv
-
-#readonly DATA_FILE="./daily.json"
-#readonly DATA_URL="https://www.coronavirus.in.gov/map/covid-19-indiana-daily-report-current.topojson"
-
 source ./covid.conf
 
 get_COVID_data(){
